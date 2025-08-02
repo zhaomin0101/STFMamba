2a5ac357c71bcb3293984ed483b131f44ed287ee
zhaomin0101 <69246551+zhaomin0101@users.noreply.github.com>
2025-08-02 19:27:42 +0800

    Update MambaSRmodel.py

--- models_mamba/MambaSRmodel.py
+++ models_mamba/MambaSRmodel.py
@@ -5,53 +5,49 @@ class MambaSRmodel(nn.Module):
     def __init__(self, channel_first=False, norm_layer="LN", ssm_act_layer="silu", mlp_act_layer="gelu", **kwargs):
         super(MambaSRmodel, self).__init__()
         self.conv_first = nn.Conv2d(6, 12, 3, 2, 1)
-        self.st_block_11 = nn.ModuleList(
-            [nn.Identity(),
-            *[VSSBlock(hidden_dim=64, drop_path=0.1, norm_layer=norm_layer, channel_first=channel_first,
-                ssm_d_state=kwargs['ssm_d_state'], ssm_ratio=kwargs['ssm_ratio'], ssm_dt_rank=kwargs['ssm_dt_rank'], ssm_act_layer=ssm_act_layer,
-                ssm_conv=kwargs['ssm_conv'], ssm_conv_bias=kwargs['ssm_conv_bias'], ssm_drop_rate=kwargs['ssm_drop_rate'], ssm_init=kwargs['ssm_init'],
-                forward_type=kwargs['forward_type'], mlp_ratio=kwargs['mlp_ratio'], mlp_act_layer=mlp_act_layer, mlp_drop_rate=kwargs['mlp_drop_rate'],
-                gmlp=kwargs['gmlp'], use_checkpoint=kwargs['use_checkpoint']) 
-                for _ in range(2)],] 
-        )
-        self.st_block_12 = nn.ModuleList(
-            [
-            *[VSSBlock(hidden_dim=12, drop_path=0.1, norm_layer=norm_layer, channel_first=channel_first,
-                ssm_d_state=kwargs['ssm_d_state'], ssm_ratio=kwargs['ssm_ratio'], ssm_dt_rank=kwargs['ssm_dt_rank'], ssm_act_layer=ssm_act_layer,
-                ssm_conv=kwargs['ssm_conv'], ssm_conv_bias=kwargs['ssm_conv_bias'], ssm_drop_rate=kwargs['ssm_drop_rate'], ssm_init=kwargs['ssm_init'],
-                forward_type=kwargs['forward_type'], mlp_ratio=kwargs['mlp_ratio'], mlp_act_layer=mlp_act_layer, mlp_drop_rate=kwargs['mlp_drop_rate'],
-                gmlp=kwargs['gmlp'], use_checkpoint=kwargs['use_checkpoint'])
-                 for _ in range(2)]]
-        )
-        self.conv_final = nn.ConvTranspose2d( in_channels=12, out_channels=6, kernel_size=4, stride=2, padding=1)
+        self.sr_block = nn.ModuleList()
+        for _ in range(2):  
+            self.sr_block.extend([
+            nn.Identity(),
+            VSSBlock(
+            hidden_dim=64, drop_path=0.1, norm_layer=norm_layer, channel_first=channel_first,
+            ssm_d_state=kwargs['ssm_d_state'], ssm_ratio=kwargs['ssm_ratio'], ssm_dt_rank=kwargs['ssm_dt_rank'], ssm_act_layer=ssm_act_layer,
+            ssm_conv=kwargs['ssm_conv'], ssm_conv_bias=kwargs['ssm_conv_bias'], ssm_drop_rate=kwargs['ssm_drop_rate'], ssm_init=kwargs['ssm_init'],
+            forward_type=kwargs['forward_type'], mlp_ratio=kwargs['mlp_ratio'], mlp_act_layer=mlp_act_layer, mlp_drop_rate=kwargs['mlp_drop_rate'],
+            gmlp=kwargs['gmlp'], use_checkpoint=kwargs['use_checkpoint']
+                ),
+            Permute(0, 2, 3, 1),
+            VSSBlock(
+            hidden_dim=12, drop_path=0.1, norm_layer=norm_layer, channel_first=channel_first,
+            ssm_d_state=kwargs['ssm_d_state'], ssm_ratio=kwargs['ssm_ratio'], ssm_dt_rank=kwargs['ssm_dt_rank'], ssm_act_layer=ssm_act_layer,
+            ssm_conv=kwargs['ssm_conv'], ssm_conv_bias=kwargs['ssm_conv_bias'], ssm_drop_rate=kwargs['ssm_drop_rate'], ssm_init=kwargs['ssm_init'],
+            forward_type=kwargs['forward_type'], mlp_ratio=kwargs['mlp_ratio'], mlp_act_layer=mlp_act_layer, mlp_drop_rate=kwargs['mlp_drop_rate'],
+            gmlp=kwargs['gmlp'], use_checkpoint=kwargs['use_checkpoint']
+                ),
+            Permute(0, 3, 1, 2),
+                ])
+        self.conv_final = nn.ConvTranspose2d( in_channels=12, out_channels=6, kernel_size=4, 
+    stride=2, padding=1)
         
 
     def forward(self, x):
         input = x
         x = self.conv_first(x)
-        #stage 1
-        for index, module in enumerate(self.st_block_11):
-            output = module(x)
-        for index, module in enumerate(self.st_block_12):
-            output = module(output.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        output = x
+        for index, module in enumerate(self.sr_block):
+            output = module(output)
         x = output+x
-        #stage 2
-        for index, module in enumerate(self.st_block_11):
-            output = module(x)
-        for index, module in enumerate(self.st_block_12):
-            output = module(output.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        output = x
+        for index, module in enumerate(self.sr_block):
+            output = module(output)
         x = output+x
-        #stage 3
-        for index, module in enumerate(self.st_block_11):
-            output = module(x)
-        for index, module in enumerate(self.st_block_12):
-            output = module(output.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        output = x
+        for index, module in enumerate(self.sr_block):
+            output = module(output)
         x = output+x
-        #stage 4
-        for index, module in enumerate(self.st_block_11):
-            output = module(x)
-        for index, module in enumerate(self.st_block_12):
-            output = module(output.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        output = x
+        for index, module in enumerate(self.sr_block):
+            output = module(output)
         output = output+x
         output = self.conv_final(output)
         output = input+output
